bd70bf0af9de863a98ac4e561c991c13feafbcea
tatiana.korliukova <tatiana.korliukova@intexsoft.by>
2020-02-26 17:55:46 +0300

    add AfretEach annotation

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,8 +1,10 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
+import static com.codeborne.selenide.Selenide.clearBrowserCookies;
 import static com.codeborne.selenide.Selenide.open;
 
 public class BaseTest {
@@ -11,5 +13,10 @@ public class BaseTest {
     public void setup() {
         Configuration.startMaximized=true;
         open("http://automationpractice.com");
+    }
+
+    @AfterEach
+    public void clearCookies() {
+        clearBrowserCookies();
     }
 }
