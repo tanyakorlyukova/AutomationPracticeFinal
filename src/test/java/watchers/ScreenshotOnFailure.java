24a55d1d6abe52f06486e6440fab7fad601ed3a1
tatiana.korliukova <tatiana.korliukova@intexsoft.by>
2020-03-04 13:06:31 +0300

    add sreenshot on failure in allure report

--- src/test/java/watchers/ScreenshotOnFailure.java
+++ src/test/java/watchers/ScreenshotOnFailure.java
@@ -1,4 +1,27 @@
 package watchers;
 
-public class ScreenshotOnFailure {
+import com.codeborne.selenide.Screenshots;
+import io.qameta.allure.Attachment;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestWatcher;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ScreenshotOnFailure implements TestWatcher {
+
+    @Override
+    public void testFailed(ExtensionContext context, Throwable cause) {
+        try {
+            makeScreenshotOnFailure(Screenshots.getLastScreenshot().getName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Attachment("Screenshot on failure")
+    public byte[] makeScreenshotOnFailure(String resourceName) throws IOException {
+            return Files.readAllBytes(Paths.get("build/reports/tests", resourceName));
+    }
 }
